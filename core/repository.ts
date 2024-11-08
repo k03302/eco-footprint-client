b432005efc041e92b7aaebc5419b9ae0b79f025d
k03302 <unisonman660@gmail.com>
2024-11-08 18:30:31 +0900

    Remane repository classes

--- core/repository.ts
+++ core/repository.ts
@@ -5,38 +5,38 @@ import {
 
 } from "./model"
 
-export interface UserRepository {
+export interface UserRepo {
     createUser(userItem: UserItem): Promise<UserItem>
-    getUserInfo(): Promise<UserItem>
+    getUserInfo(userId: string): Promise<UserItem>
     updateUserInfo(userItem: UserItem): Promise<UserItem>
     deleteUser(userId: string): Promise<boolean>
 }
 
-export interface CouponRepository {
+export interface CouponRepo {
     getCoupon(couponId: string): Promise<CouponItem>
     getNewCoupon(userId: string, rewardId: string): Promise<CouponItem>
     updateCoupon(couponItem: CouponItem): Promise<CouponItem>
 }
 
-export interface ChallengeRepository {
+export interface ChallengeRepo {
     createChallenge(challengeItem: ChallengeItem): Promise<ChallengeItem>
     getAllChallenges(): Promise<ChallengeItemMeta[]>
     updateChallenge(challengeItem: ChallengeItem): Promise<ChallengeItem>
     deleteChallenge(challengeId: string): Promise<boolean>
 }
 
-export interface DonationRepository {
+export interface DonationRepo {
     getAllDonations(): Promise<DonationItemMeta[]>
     getDonation(donationId: string): Promise<DonationItem>
     updateDonation(donationItem: DonationItem): Promise<DonationItem>
 }
 
-export interface RewardRepository {
+export interface RewardRepo {
     getAllRewards(): Promise<RewardItemMeta[]>
     getRewardInfo(rewardId: string): Promise<RewardItem>
 }
 
-export interface FileRepository {
+export interface FileRepo {
     uploadFile(file: FileInput): Promise<FileData>
     updateFile(file: FileInput): Promise<FileData>
     deleteFile(fileId: FileInput): Promise<FileData>
