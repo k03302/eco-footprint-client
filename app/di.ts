e6e98fe4297ca1e43cf93f398f1323455f220806
k03302 <unisonman660@gmail.com>
2024-11-11 15:46:19 +0900

    Write di

--- app/di.ts
+++ app/di.ts
@@ -1,12 +1,21 @@
-/* Interface */
+import { UserRepo, CouponRepo, ChallengeRepo, DonationRepo, RewardRepo, FileRepo } from '@/core/repository';
+import ChallengeLocalRepo from "@/repo/local-repo/challenge-local-repo";
+import CouponLocalRepo from "@/repo/local-repo/coupon-local-repo";
+import DonationLocalRepo from '@/repo/local-repo/donation-local-repo';
+import FileLocalRepo from "@/repo/local-repo/file-local-repo";
+import RewardLocalRepo from "@/repo/local-repo/reward-local-repo";
+import UserLocalRepo from "@/repo/local-repo/user-local-repo";
 
-/* Repository */
 
-/* Utils */
 
-/* Dependency Injection */
+const challenges: ChallengeRepo = new ChallengeLocalRepo();
+const coupons: CouponRepo = new CouponLocalRepo();
+const donations: DonationRepo = new DonationLocalRepo();
+const files: FileRepo = new FileLocalRepo();
+const rewards: RewardRepo = new RewardLocalRepo();
+const users: UserLocalRepo = new UserLocalRepo();
 
-/* Export */
-export const repo = {};
 
-export const util = {};
+export const repo = {
+    challenges, coupons, donations, files, rewards, users
+};
