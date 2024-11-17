72cc9979e20c1bbedb0c43aa51a50def58ca0d93
k03302 <unisonman660@gmail.com>
2024-11-17 21:09:26 +0900

    Add ad api

---
+++ api/ad.ts
@@ -0,0 +1,83 @@
+import {
+    RewardedAd,
+    RewardedAdEventType,
+    RewardedAdReward,
+    TestIds,
+} from 'react-native-google-mobile-ads';
+
+
+const adUnitId: string = TestIds.REWARDED || process.env.EXPO_PUBLIC_ADMOB_REWARDED_AD_KEY!;
+
+class AdService {
+    private rewarded: RewardedAd | null = null;
+    private loadedHandler: () => void = () => { };
+    private earnedhandler: (payload: RewardedAdReward) => void = (payload: RewardedAdReward) => { };
+    private showAdOnLoad: boolean = false;
+    private loaded: boolean = false;
+    private onLoading: boolean = false;
+
+    isAdOnLoading(): boolean {
+        return this.onLoading;
+    }
+
+    isAdLoaded(): boolean {
+        return this.loaded;
+    }
+
+    showAd(showAdOnLoad: boolean = false): boolean {
+        if (this.loaded && this.rewarded) {
+            this.rewarded.show();
+            return true;
+        } else if (showAdOnLoad) {
+            this.showAdOnLoad = true;
+        }
+        return false;
+    }
+
+    loadAd() {
+        console.log('loadAd method');
+        this.onLoading = true;
+        this.rewarded = RewardedAd.createForAdRequest(adUnitId);
+
+        this.rewarded.addAdEventListener(
+            RewardedAdEventType.LOADED,
+            () => {
+                console.log('ad loaded');
+                this.onLoading = false;
+                if (this.showAdOnLoad) {
+                    this.showAdOnLoad = false;
+                    this.loaded = false;
+                    this.rewarded?.show();
+                } else {
+                    this.loaded = true;
+                }
+                this.loadedHandler();
+            }
+        );
+
+        // 라워드를 받았을 때 이벤트 리스너
+        this.rewarded.addAdEventListener(
+            RewardedAdEventType.EARNED_REWARD,
+            (payload) => {
+                console.log('reward earned', payload);
+                this.rewarded?.removeAllListeners();
+                this.loaded = false;
+                this.showAdOnLoad = false;
+                this.earnedhandler(payload);
+            }
+        );
+
+        this.rewarded.load();
+    }
+
+    registerLoadedHandler(loadedHandler: () => void) {
+        this.loadedHandler = loadedHandler;
+    }
+
+    registerEarnedHandler(earnedHandler: (payload: RewardedAdReward) => void) {
+        this.earnedhandler = earnedHandler;
+    }
+}
+
+
+export const adService = new AdService();
