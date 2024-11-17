e0ca4193600c7b98d588cd20bbadeb9c82f67988
k03302 <unisonman660@gmail.com>
2024-11-17 21:13:14 +0900

    Update kakaologin page

--- app/(auth)/kakaologin.tsx
+++ app/(auth)/kakaologin.tsx
@@ -4,72 +4,66 @@ import { View, StyleSheet } from "react-native";
 import { WebView } from 'react-native-webview';
 import { router } from 'expo-router'
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { kakaoLogin } from '@/api/auth'
 
 
 
 
-const REST_API_KEY = 'a0f7848c5e09023c767195b1b09be8a9';
-const REDIRECT_URI = 'http://localhost:8081';
+
+const REST_API_KEY = process.env.EXPO_PUBLIC_KAKAO_REST_API_KEY!;
+const REDIRECT_URI = process.env.EXPO_PUBLIC_REDIRECT_URI!;
 const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 const KaKaoLogin = () => {
-    const [accessToken, setAccessToken] = useState<string | null>(null);
-    const [idToken, setIdToken] = useState<string | null>(null);
+    const [showButton, setShowButton] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
+    const messageHandler = async (event: any) => {
+        const data = event.nativeEvent.url;
+        const authCode = getAuthCode(data);
+        if (!authCode) return;
+        await kakaoLogin.login(authCode);
+        if (await kakaoLogin.isRegistered()) {
+            router.replace('/map');
+        } else {
+            router.replace('/register');
+        }
+    }
 
-    const getCode = (target: string) => {
+    const getAuthCode = (target: string) => {
         const exp = 'code=';
         const condition = target.indexOf(exp);
         if (condition !== -1) {
             const requestCode = target.substring(condition + exp.length);
             console.log('access code: ', requestCode);
-            requestToken(requestCode);
+            setShowButton(false);
+            return requestCode;
         }
+
+        const errorExp = 'error=';
+        const errorCondition = target.indexOf(errorExp);
+        if (errorCondition !== -1) {
+            setError(target);
+        }
+        return null;
     };
-
-    const requestToken = async (authorize_code: string) => {
-        axios({
-            method: 'post',
-            url: 'https://kauth.kakao.com/oauth/token',
-            params: {
-                grant_type: 'authorization_code',
-                client_id: REST_API_KEY,
-                redirect_uri: REDIRECT_URI,
-                code: authorize_code
-            }
-        }).then((res) => {
-            setAccessToken(res.data.access_token);
-            setIdToken(res.data.access_token);
-            console.log("id token: ", idToken);
-            console.log("access token: ", accessToken);
-        }).catch(function (error) {
-            console.log('error', error);
-        })
-    };
-
-    useEffect(() => {
-        router.push('/map');
-    }, [idToken])
-
 
 
     return (
         <View style={Styles.container}>
-            <WebView
-                style={{ flex: 1 }}
-                originWhitelist={['*']}
-                scalesPageToFit={false}
-                source={{
-                    uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
-                }}
-                injectedJavaScript={INJECTED_JAVASCRIPT}
-                javaScriptEnabled
-                onMessage={event => {
-                    const data = event.nativeEvent.url;
-                    getCode(data);
-                }}
-            />
+            {
+                showButton ? <WebView
+                    style={{ flex: 1 }}
+                    originWhitelist={['*']}
+                    scalesPageToFit={false}
+                    source={{
+                        uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
+                    }}
+                    injectedJavaScript={INJECTED_JAVASCRIPT}
+                    javaScriptEnabled
+                    onMessage={messageHandler}
+                /> : <></>
+            }
         </View>
     )
 }
