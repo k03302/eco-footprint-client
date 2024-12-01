56eb50238093b8f4c4db0b40296814fb85d5f98f
k03302 <unisonman660@gmail.com>
2024-12-01 16:26:57 +0900

    Add a button component

---
+++ components/GreenButton.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import {
+    Text,
+    StyleSheet,
+    Pressable,
+    PressableProps,
+    ViewStyle,
+    ActivityIndicator,
+} from 'react-native';
+
+interface ButtonProps extends PressableProps {
+    title: string;
+    style?: ViewStyle;
+    variant?: 'primary' | 'secondary';
+}
+
+const GreenButton: React.FC<ButtonProps> = ({
+    title,
+    style,
+    variant = 'primary',
+    ...props
+}) => {
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <Pressable
+            style={({ pressed }) => [
+                styles.button,
+                variant === 'primary' ? styles.primary : styles.secondary,
+                pressed && styles.pressed,
+                style,
+            ]}
+            onPress={props.onPress}
+            disabled={loading}
+            {...props}
+        >
+            {loading ? (
+                <ActivityIndicator color="#fff" />
+            ) : (
+                <Text style={styles.text}>{title}</Text>
+            )}
+        </Pressable>
+    );
+};
+
+export default GreenButton;
+
+const styles = StyleSheet.create({
+    button: {
+        paddingVertical: 12,
+        paddingHorizontal: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 10,
+    },
+    primary: {
+        backgroundColor: '#849C6A',
+    },
+    secondary: {
+        backgroundColor: '#849C6A',
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
