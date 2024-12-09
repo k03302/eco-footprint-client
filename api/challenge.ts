ee1929bf4750ac6f33560c6bf46c49bc75d1aab0
k03302 <unisonman660@gmail.com>
2024-12-10 00:48:45 +0900

    Update api

--- api/challenge.ts
+++ api/challenge.ts
@@ -1,2 +1,24 @@
+import { ChallengeItem } from '@/core/model';
 import { filePost, axiosPost, axiosGet, axiosPut, axiosDelete } from '@/utils/axios';
 import { getUserId } from '@/utils/login';
+
+
+export async function createChallenge(challengeInfo: ChallengeItem): Promise<ChallengeItem | null> {
+    return axiosPost('challenge/create', challengeInfo);
+}
+
+export async function getAllChallenges() {
+    return axiosGet('challenge/all');
+}
+
+export async function getChallenge(challengeId: string) {
+    return axiosGet('challenge/' + challengeId);
+}
+
+export async function participateChallenge(challengeId: string): Promise<ChallengeItem | null> {
+    return axiosPost('challenge/' + challengeId + '/participate', {});
+}
+
+export async function addChallengeRecord(challengeId: string, recordId: string) {
+
+}
--- api/reward.ts
+++ api/reward.ts
@@ -1,2 +1,38 @@
+import { CouponItem, RewardItem } from '@/core/model';
 import { filePost, axiosPost, axiosGet, axiosPut, axiosDelete } from '@/utils/axios';
 import { getUserId } from '@/utils/login';
+
+
+export async function createReward(rewardInfo: RewardItem) {
+    return axiosPost('reward/create', rewardInfo);
+}
+
+export async function getAllRewards() {
+    return axiosGet('reward/all');
+}
+
+export async function getReward(rewardId: string) {
+    return axiosGet('reward/' + rewardId);
+}
+
+export async function updateReward(rewardId: string, update: (rewardInfo: RewardItem) => RewardItem) {
+
+}
+
+export async function deleteReward(rewardId: string) {
+    const result = await axiosDelete('reward/delete/' + rewardId);
+    return result !== null;
+}
+
+export async function perchase(rewardId: string) {
+    return axiosPost('reward/purchase/' + rewardId, {});
+}
+
+export async function extendCoupon(couponId: string): Promise<CouponItem | null> {
+    return axiosPost('reward/extend/' + couponId, {});
+}
+
+export async function deleteCoupon(couponId: string): Promise<boolean> {
+    const result = await axiosDelete('reward/delete/' + couponId);
+    return result !== null;
+}
