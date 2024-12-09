1908326b74593f1deeeadc4173b800c588637a74
k03302 <unisonman660@gmail.com>
2024-12-10 00:48:00 +0900

    Update utils

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,66 +1,104 @@
 import axios from 'axios';
 import { getIdToken } from './login';
+import * as FileSystem from 'expo-file-system';
 
-export async function axiosGet(path: string) {
-    console.log(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`);
-    console.log(await getIdToken());
-    await axios.get(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`, {
+const apiRoot = process.env.EXPO_PUBLIC_SERVER_API!;
+
+export async function filePost(path: string, fileUri: string): Promise<any> {
+
+    const fullPath = apiRoot + path;
+    const idToken = await getIdToken();
+    console.log("[filePost]", fullPath, idToken);
+
+    return FileSystem.uploadAsync(apiRoot + path, fileUri, {
+        httpMethod: 'POST',
+        fieldName: 'file',
+        uploadType: FileSystem.FileSystemUploadType.MULTIPART,
         headers: {
-            Authorization: `Bearer ${await getIdToken()}`
+            Authorization: `Bearer ${idToken}`
+        }
+    }).then(response => {
+        const data = JSON.parse(response.body);
+        console.log("---->[response] success");
+        return data;
+    }).catch(error => {
+        console.log("---->[error]", error.status, error.message);
+        return null;
+    });
+}
+
+export async function axiosGet(path: string): Promise<any> {
+    const fullPath = apiRoot + path;
+    const idToken = await getIdToken();
+    console.log("[axiosGet]", fullPath, idToken);
+
+
+    return axios.get(fullPath, {
+        headers: {
+            Authorization: 'Bearer ' + idToken
         }
     }).then(response => {
         console.log(response.data);
         return response.data;
     }).catch(error => {
-        console.error(error);
+        console.error(error.response.data.message, error.response.status);
+        return null;
     });
-    return null;
 }
 
 export async function axiosPost(path: string, body: any) {
-    console.log(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`, body);
-    console.log(await getIdToken());
-    await axios.post(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`, body, {
+    const fullPath = apiRoot + path;
+    const idToken = await getIdToken();
+    console.log("[axiosPost]", fullPath, idToken);
+
+
+    return axios.post(fullPath, body, {
         headers: {
-            Authorization: `Bearer ${await getIdToken()}`
+            Authorization: 'Bearer ' + idToken
         }
     }).then(response => {
         console.log(response.data);
         return response.data;
     }).catch(error => {
-        console.error(error);
-    })
-    return null;
+        console.error(error.response.data.message, error.response.status);
+        return null;
+    });
 }
 
 export async function axiosPut(path: string, body: any) {
-    console.log(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`, body);
-    console.log(await getIdToken());
-    await axios.put(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`, body, {
+    const fullPath = apiRoot + path;
+    const idToken = await getIdToken();
+    console.log("[axiosPut]", fullPath, idToken);
+
+
+    return axios.put(fullPath, body, {
         headers: {
-            Authorization: `Bearer ${await getIdToken()}`
+            Authorization: 'Bearer ' + idToken
         }
     }).then(response => {
         console.log(response.data);
         return response.data;
     }).catch(error => {
-        console.error(error);
-    })
-    return null;
+        console.error(error.response.data.message, error.response.status);
+        return null;
+    });
 }
 
 export async function axiosDelete(path: string) {
-    console.log(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`);
-    console.log(await getIdToken());
-    await axios.delete(`${process.env.EXPO_PUBLIC_SERVER_API!}${path}`, {
+    const fullPath = apiRoot + path;
+    const idToken = await getIdToken();
+    console.log("[axiosDelete]", fullPath, idToken);
+
+
+    return axios.get(fullPath, {
         headers: {
-            Authorization: `Bearer ${await getIdToken()}`
+            Authorization: 'Bearer ' + idToken
         }
     }).then(response => {
         console.log(response.data);
         return response.data;
     }).catch(error => {
-        console.error(error);
-    })
-    return null;
+        console.error(error.response.data.message, error.response.status);
+        return null;
+    });
 }
--- utils/login.ts
+++ utils/login.ts
@@ -1,28 +1,39 @@
-import { NO_USER } from '@/core/model';
+import { FileData, FileInput, NO_USER, UserItem } from '@/core/model';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { router } from 'expo-router';
 import { Alert } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 const ID_TOKEN_KEY = 'id_token';
+const USER_ID_KEY = 'user_id';
 
 
 let idTokenCache: string | null = null;
+let userIdCache: string | null = null;
 
-// register api url
-const url = `${process.env.EXPO_PUBLIC_SERVER_API!}${'user/register'}`;
+
+const apiRoot = process.env.EXPO_PUBLIC_SERVER_API!;
+const registerUrl = apiRoot + 'user/register';
+const updateUserUrl = apiRoot + 'user/profile/';
+const uploadFileUrl = apiRoot + 'file/create';
 
 export async function login({ idToken, useStoredToken = false }: { idToken?: string, useStoredToken?: boolean }) {
     // 저장된 토큰 사용하지 않고, idToken 제공 하지 않은 경우 리턴
     if (!useStoredToken && !idToken) return;
 
     // useStoredToken 값에 따라 저장된 값을 가져오거나 idToken값을 사용
-    const token = useStoredToken ? await getIdToken() : (idToken || "");
+    const token = useStoredToken ? await getIdToken() : idToken;
+    if (!token) {
+        Alert.alert("로그인에 실패했습니다.");
+        router.replace('/');
+        return;
+    }
 
     // 서버에 register 요청. data는 더미 데이터 제공
-    await axios.post(url, {
+    await axios.post(registerUrl, {
         id: token,
-        username: "asdf",
+        username: "",
         point: 0,
         challengeList: [],
         couponList: [],
@@ -32,15 +43,27 @@ export async function login({ idToken, useStoredToken = false }: { idToken?: str
             Authorization: `Bearer ${token}`
         }
     }).then(res => {
-        // 더미 데이터로 register됨. register페이지로 이동
-        AsyncStorage.setItem(ID_TOKEN_KEY, token);
+        const userInfo = res.data as UserItem;
+        userIdCache = userInfo.id;
+        return AsyncStorage.setItem(USER_ID_KEY, userInfo.id);
+    }).then(() => {
+        idTokenCache = token;
+        return AsyncStorage.setItem(ID_TOKEN_KEY, token);
+    }).then(() => {
         router.replace('/register');
     }).catch(error => {
         console.log(error.response);
         if (error.response.status === 409) {
-            // 이미 사용자가 존재하여 더미 데이터를 등록할 수 없음. 로그인 성공
-            AsyncStorage.setItem(ID_TOKEN_KEY, token);
-            router.replace('/map');
+            idTokenCache = token;
+            AsyncStorage.setItem(ID_TOKEN_KEY, token)
+                .then(() => {
+                    userIdCache = token;
+                    console.log("userIdCache", userIdCache);
+                    return AsyncStorage.setItem(USER_ID_KEY, token);
+                }).then(() => {
+                    router.replace('/map');
+                });
+
         } else {
             Alert.alert("로그인에 실패했습니다.");
             router.replace('/');
@@ -51,34 +74,76 @@ export async function login({ idToken, useStoredToken = false }: { idToken?: str
 export async function register({ username, thumbnailUri, idToken, useStoredToken = false }
     : { username: string, thumbnailUri: string, idToken?: string, useStoredToken?: boolean }) {
     if (!useStoredToken && !idToken) return;
-    const token = (useStoredToken ? await getIdToken() : idToken) || "";
+    const token = useStoredToken ? await getIdToken() : idToken;
+    if (!token) {
+        Alert.alert("회원가입에 실패했습니다.");
+        router.replace('/');
+        return;
+    }
+
 
 
 
     // 서버에 register 요청. data는 더미 데이터 제공
-    await axios.post(url, {
+    await axios.post(registerUrl, {
         id: token,
-        username: username,
+        username: "",
         point: 0,
         challengeList: [],
         couponList: [],
-        thumbnailId: thumbnailUri
+        thumbnailId: ""
     }, {
         headers: {
             Authorization: `Bearer ${token}`
         }
     }).then(res => {
-        // 더미 데이터로 register됨. register페이지로 이동
-        AsyncStorage.setItem(ID_TOKEN_KEY, token);
-        router.replace('/register');
+        const userInfo = res.data as UserItem;
+        userIdCache = userInfo.id;
+        console.log("userIdCache", userIdCache);
+        return AsyncStorage.setItem(USER_ID_KEY, userInfo.id);
+    }).then(() => {
+        idTokenCache = token;
+        return AsyncStorage.setItem(ID_TOKEN_KEY, token);
+    }).then(() => {
+        return FileSystem.uploadAsync(uploadFileUrl, thumbnailUri, {
+            httpMethod: 'POST',
+            fieldName: 'file',
+            uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    }).then(response => {
+        const imgData = JSON.parse(response.body) as FileData;
+        const userId = getUserId();
+        return axios.post(updateUserUrl + userId, {
+            id: token,
+            username: username,
+            point: 0,
+            challengeList: [],
+            couponList: [],
+            thumbnailId: imgData.id
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    }).then(() => {
+        router.replace('/map');
     }).catch(error => {
         console.log(error.response);
         if (error.response.status === 409) {
-            // 이미 사용자가 존재하여 더미 데이터를 등록할 수 없음. 로그인 성공
-            AsyncStorage.setItem(ID_TOKEN_KEY, token);
-            router.replace('/map');
+            idTokenCache = token;
+            AsyncStorage.setItem(ID_TOKEN_KEY, token)
+                .then(() => {
+                    userIdCache = token;
+                    console.log("userIdCache", userIdCache);
+                    return AsyncStorage.setItem(USER_ID_KEY, token)
+                }).then(() => {
+                    router.replace('/map');
+                });
         } else {
-            Alert.alert("로그인에 실패했습니다.");
+            Alert.alert("회원가입에 실패했습니다.");
             router.replace('/');
         }
     });
@@ -86,12 +151,24 @@ export async function register({ username, thumbnailUri, idToken, useStoredToken
 
 
 
-export async function getIdToken(): Promise<string> {
-    return idTokenCache || (await AsyncStorage.getItem(ID_TOKEN_KEY)) || "";
+export async function getIdToken(): Promise<string | null> {
+    if (!idTokenCache) {
+        idTokenCache = await AsyncStorage.getItem(ID_TOKEN_KEY);
+    }
+    return idTokenCache;
+}
+
+export async function getUserId(): Promise<string | null> {
+    if (!userIdCache) {
+        userIdCache = await AsyncStorage.getItem(USER_ID_KEY);
+    }
+    return userIdCache;
 }
 
 
 export async function logout() {
-    AsyncStorage.removeItem(ID_TOKEN_KEY);
+    await AsyncStorage.removeItem(ID_TOKEN_KEY);
+    await AsyncStorage.removeItem(USER_ID_KEY);
     idTokenCache = null;
+    userIdCache = null;
 }
