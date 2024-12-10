4005488bd3c4d2f70671b5193df3feade540c67d
k03302 <unisonman660@gmail.com>
2024-12-11 06:39:21 +0900

    Update model

--- core/model.ts
+++ core/model.ts
@@ -1,14 +1,25 @@
-export type ChallengeItemMeta = {
-    id: string;
-    name: string;
-    totalParticipants: number;
-    currentParticipants: number;
-    dateEnd: Date;
+
+export enum ItemState {
+    UNDEFINED = -1,
+    PENDING = 0,
+    ACTIVE = 1,
+    INACTIVE = 2,
+    FINISHED = 3,
+    FAILED = 4
 }
+
 
 export type UserItemMeta = {
     id: string;
     username: string;
+    thumbnailId: string;
+}
+
+export type DonationItemMeta = {
+    id: string;
+    name: string;
+    currentPoint: number;
+    totalPoint: number;
     thumbnailId: string;
 }
 
@@ -20,28 +31,20 @@ export type CouponItemMeta = {
     expiredAt: string;
 }
 
-export type ChallengeRecoordItem = {
-    userId: string;
-    recordId: string;
-    uploadDate: Date;
-    approved: boolean;
-}
-
-export enum ItemState {
-    UNDEFINED = -1,
-    PENDING = 0,
-    ACTIVE = 1,
-    INACTIVE = 2,
-    FINISHED = 3,
-    FAILED = 4
-}
-
-export type DonationItemMeta = {
+export type ChallengeItemMeta = {
     id: string;
     name: string;
-    currentPoint: number;
-    totalPoint: number;
-    thumbnailId: string;
+    totalParticipants: number;
+    currentParticipants: number;
+    dateEnd: Date;
+}
+
+export type ChallengeRecordItem = {
+    id: string;
+    userId: string;
+    imageId: string;
+    date: string;
+    approved: boolean;
 }
 
 export type RewardItemMeta = {
@@ -53,22 +56,8 @@ export type RewardItemMeta = {
     thumbnailId: string;
 }
 
-export type FileData = {
-    id: string;
-    owner: string;
-    name: string;
-    size: number;
-    contentType: string;
-    file: string;
-    isPrivate: boolean;
-}
 
-export type FileInput = {
-    id: string;
-    name: string;
-    fileUri: string;
-    localLocation?: boolean;
-}
+
 
 
 
@@ -77,9 +66,18 @@ export type UserItem = {
     id: string;
     username: string;
     point: number;
-    challengeList: ChallengeItemMeta[];
     couponList: CouponItemMeta[];
     thumbnailId: string;
+}
+
+export type DonationItem = {
+    id: string;
+    name: string;
+    currentPoint: number;
+    totalPoint: number;
+    description: string;
+    thumbnailId: string;
+    state: ItemState;
 }
 
 export type CouponItem = {
@@ -92,29 +90,17 @@ export type CouponItem = {
     couponId: string;
 }
 
-
 export type ChallengeItem = {
     id: string;
     name: string;
     totalParticipants: number;
     currentParticipants: number;
-    createdBy: string;
+    createdAt: string;
     participants: UserItemMeta[];
-    participantsRecord: ChallengeRecoordItem[];
+    participantsRecords: ChallengeRecordItem[];
     dateStart: Date;
     dateEnd: Date;
     description: string;
-}
-
-
-
-export type DonationItem = {
-    id: string;
-    name: string;
-    currentPoint: number;
-    totalPoint: number;
-    description: string;
-    thumbnailId: string;
     state: ItemState;
 }
 
@@ -124,74 +110,21 @@ export type RewardItem = {
     brandName: string;
     itemType: string;
     description: string;
-    price: number;
     imageId: string;
     thumbnailId: string;
+    price: number;
     provider: string;
 }
 
 
 
-export const NO_USER: UserItem = {
-    id: "",
-    username: "",
-    point: 0,
-    challengeList: [],
-    couponList: [],
-    thumbnailId: ""
+export type FileData = {
+    id: string;
+    owner: string;
+    name: string;
+    size: number;
+    contentType: string;
+    file: string;
+    isPrivate: boolean;
 }
 
-export const NO_COUPON: CouponItem = {
-    id: "",
-    itemName: "",
-    brandName: "",
-    thumbnailId: "",
-    expiredAt: "",
-    description: "",
-    couponId: ""
-}
-
-export const NO_CHALLENGE: ChallengeItem = {
-    id: "",
-    name: "",
-    totalParticipants: 0,
-    currentParticipants: 0,
-    createdBy: "",
-    participants: [],
-    participantsRecord: [],
-    dateStart: new Date(),
-    dateEnd: new Date(),
-    description: ""
-}
-
-export const NO_REWARD: RewardItem = {
-    id: "",
-    itemName: "",
-    brandName: "",
-    itemType: "",
-    description: "",
-    price: 0,
-    imageId: "",
-    thumbnailId: "",
-    provider: ""
-}
-
-export const NO_DONATION: DonationItem = {
-    id: "",
-    name: "",
-    currentPoint: 0,
-    totalPoint: 0,
-    description: "",
-    thumbnailId: "",
-    state: 0
-}
-
-export const NO_FILE: FileData = {
-    id: "",
-    name: "",
-    size: 0,
-    contentType: "",
-    owner: "",
-    file: "",
-    isPrivate: false
-}
